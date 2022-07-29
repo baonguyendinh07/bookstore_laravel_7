8d64869f469239abc380ed4fe6e727610556d2ae
bao nguyen <bao.nguyendinh07@gmail.com>
2022-07-29 22:45:57 +0700

    delete asset

--- resources/views/admin/html/script.blade.php
+++ resources/views/admin/html/script.blade.php
@@ -1,38 +1,38 @@
 <!-- jQuery -->
-<script src="{{ url('public/admin/plugins/jquery/jquery.min.js')}}"></script>
+<script src="/public/admin/plugins/jquery/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="{{ url('public/admin/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<script src="/public/admin/plugins/jquery-ui/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
     $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="{{ url('public/admin/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="/public/admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- ChartJS -->
-<script src="{{ url('public/admin/plugins/chart.js/Chart.min.js')}}"></script>
+<script src="/public/admin/plugins/chart.js/Chart.min.js"></script>
 <!-- Sparkline -->
-<script src="{{ url('public/admin/plugins/sparklines/sparkline.js')}}"></script>
+<script src="/public/admin/plugins/sparklines/sparkline.js"></script>
 <!-- JQVMap -->
-<script src="{{ url('public/admin/plugins/jqvmap/jquery.vmap.min.js')}}"></script>
-<script src="{{ url('public/admin/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
+<script src="/public/admin/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="/public/admin/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
 <!-- jQuery Knob Chart -->
-<script src="{{ url('public/admin/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
+<script src="/public/admin/plugins/jquery-knob/jquery.knob.min.js"></script>
 <!-- daterangepicker -->
-<script src="{{ url('public/admin/plugins/moment/moment.min.js')}}"></script>
-<script src="{{ url('public/admin/plugins/daterangepicker/daterangepicker.js')}}"></script>
+<script src="/public/admin/plugins/moment/moment.min.js"></script>
+<script src="/public/admin/plugins/daterangepicker/daterangepicker.js"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="{{ url('public/admin/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+<script src="/public/admin/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
 <!-- Summernote -->
-<script src="{{ url('public/admin/plugins/summernote/summernote-bs4.min.js')}}"></script>
+<script src="/public/admin/plugins/summernote/summernote-bs4.min.js"></script>
 <!-- overlayScrollbars -->
-<script src="{{ url('public/admin/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+<script src="/public/admin/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
-<script src="{{ url('public/admin/dist/js/adminlte.js')}}"></script>
+<script src="/public/admin/dist/js/adminlte.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{ url('public/admin/dist/js/pages/dashboard.js')}}"></script>
+<script src="/public/admin/dist/js/pages/dashboard.js"></script>
 <!-- Notify -->
-<script src="{{ url('public/admin/dist/js/notify.min.js')}}"></script>
+<script src="/public/admin/dist/js/notify.min.js"></script>
 <!-- Sweet Alert 2 -->
-<script src="{{ url('public/admin/plugins/sweetalert2/sweetalert2.min.js')}}"></script>
+<script src="/public/admin/plugins/sweetalert2/sweetalert2.min.js"></script>
 <!-- My Script -->
-<script src="{{ url('public/admin/dist/js/myscript.js')}}"></script>
+<script src="/public/admin/dist/js/myscript.js"></script>
