86c97ba093e9182a7b5b7ff920fa0f92ff45fbf9
bao nguyen <bao.nguyendinh07@gmail.com>
2022-07-29 15:49:42 +0700

    new laravel project

--- app/Slider.php
+++ app/Slider.php
@@ -6,7 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Slider extends Model
 {
-    protected $table = 'sliders';
-
     protected $fillable = ['id', 'name', 'description', 'picture', 'link', 'status', 'ordering', 'created_at', 'created_by', 'updated_at', 'updated_by', 'status'];
 }
